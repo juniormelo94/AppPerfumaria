1f6dbc6175c868673efb24d41910670fb7a304ea
Melo Junior <junior.melo@consultoriahl.com.br>
2025-11-28 14:49:14 -0300

    Adicionando verificacoes no menu area de vendas

--- AppPerfumaria/Services/UsuarioService.cs
+++ AppPerfumaria/Services/UsuarioService.cs
@@ -15,52 +15,68 @@ namespace AppPerfumaria.Services
     {
         private Users? _usuario { get; set; }
         private List<string> _permissoes { get; set; } = new();
+        private List<string> _instalacoes_ids { get; set; } = new();
 
         /// <summary>
         /// Carrega dados do usuário + permissões
         /// </summary>
         public async Task Set(Auth auth)
         {
-            try
+            var reinoCompany = new ReinoCompany();
+            var response = await reinoCompany.UsuariosBuscar(auth!.token!, auth!.data!.id);
+
+            if (response!.status)
             {
-                var reinoCompany = new ReinoCompany();
-                var response = await reinoCompany.UsuariosBuscar(auth!.token!, auth!.data!.id);
+                _usuario = response.data;
 
-                if (response!.status)
+                if(_usuario != null && _usuario.colaborador_user != null)
                 {
-                    _usuario = response.data;
+                    // Setando instalações do usuário
+                    _instalacoes_ids = _usuario.colaborador_user!.instalacoes_ids!;
 
-                    if(_usuario != null && _usuario.colaborador_user != null)
+                    // Setando permissoes do usuário
+                    if (_usuario.colaborador_user.tipo_user != null && _usuario.colaborador_user.tipo_user.permissoes != null)
                     {
-                        // Setando permissoes do usuário
-                        if (_usuario.colaborador_user.tipo_user != null && _usuario.colaborador_user.tipo_user.permissoes != null)
-                        {
-                            _permissoes = _usuario.colaborador_user.tipo_user.permissoes;
-                        }
+                        _permissoes = _usuario.colaborador_user.tipo_user.permissoes;
                     }
                 }
-                else
-                {
-                    throw new Exception("Tivemos um problema ao tentar setar os dados do usuário!");
-                }
             }
-            catch (Exception e)
+            else
             {
                 throw new Exception("Tivemos um problema ao tentar setar os dados do usuário!");
             }
         }
 
         /// <summary>
-        /// Verifica se o usuário tem uma permissão
+        /// Verifica se o usuário tem as permissão ou permissões
         /// </summary>
-        public bool CheckPermissao(string chave)
+        public bool CheckPermissoes(List<string>? chaves)
         {
-            if (string.IsNullOrWhiteSpace(chave))
+            if (chaves != null && chaves.Count() > 0)
             {
-                return false;
+                foreach (var chave in chaves)
+                {
+                    if (!string.IsNullOrWhiteSpace(chave) && _permissoes.Contains(chave))
+                    {
+                        return true;
+                    }
+                }
             }
 
-            return _permissoes.Contains(chave);
+            return false;
+        }
+
+        /// <summary>
+        /// Contar quantas instalações o usuário tem acesso
+        /// </summary>
+        public int CountInstalacoes()
+        {
+            if (_instalacoes_ids != null)
+            {
+                return _instalacoes_ids.Count();
+            }
+
+            return 0;
         }
     }
 }
