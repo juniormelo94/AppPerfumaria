74906353c0037e43302c469b598984afc3888b99
Junior Melo <juniormelo94@hotmail.com>
2026-03-16 22:56:53 -0300

    Adicionando IA Gemini

---
+++ AppPerfumaria/Api/GemineAI/GeminiAI.cs
@@ -0,0 +1,34 @@
+﻿using Google.GenAI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace AppPerfumaria.Api.GemineAI
+{
+    public class GeminiAI
+    {
+        const string _api_key = "AIzaSyCSAPiGovtYbUac-cUdwo3lCGVoyhL0VnQ";
+
+        public async Task<string> GenerateContent(object content)
+        {
+            var json = JsonSerializer.Serialize(content);
+
+            // The client gets the API key from the environment variable `GEMINI_API_KEY`.
+            var client = new Client(apiKey: _api_key);
+            var response = await client.Models.GenerateContentAsync(
+                model: "gemini-3-flash-preview", 
+                contents: json
+            );
+
+           return response.Candidates[0].Content.Parts[0].Text;
+
+
+            //var result = await response.Content.ReadAsStringAsync();
+
+            //var data = JsonSerializer.Deserialize<GeminiResponse>(result);
+        }
+    }
+}
